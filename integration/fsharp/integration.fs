e75fdd207b862117301b42db16c5e5864fa501b4
George K. Thiruvathukal <gkt@cs.luc.edu>
2013-02-18 10:59:12 -0600

    some cleanup of F# version

--- integration/fsharp/integration.fs
+++ integration/fsharp/integration.fs
@@ -1,21 +1,22 @@
-module sigcse
+module integration = 
+  type Fx = double -> double
 
-type Fx = double -> double
+  let integrate( a : double)(b : double)(g : int)(f : Fx) : double = 
+    let interval = (b - a) / (double) g
+    let fxValues = seq { for n in 1 .. g-1 -> f(a + (double)n * interval) }
+    let fxSum = Seq.fold (+) 0.0 fxValues
+    (b - a) / (double) g * (f(a) / 2.0 + f(b) / 2.0 + fxSum)
 
-let xSquared x : double = x * x
+module test1 = 
 
-let FxValues (a : double)(b : double)(g : int)(f : Fx) =
-   let interval = (b - a) / (double) g
-   seq { for n in 1 .. g-1 do yield f(a + (double)n * interval) }
+  let xSquared x : double = x * x
+  open integration
+  let area = integrate 0.0 10.0 1000 xSquared
+  printfn "Area = %f" area
 
-let integrate( a : double)(b : double)(g : int)(f : Fx) : double = 
-   let FxValues =
-      let interval = (b - a) / (double) g
-      seq { for n in 1 .. g-1 do yield f(a + (double)n * interval) }
-   let fxSum = Seq.fold (+) 0.0 FxValues
-   (b - a) / (double) g * (f(a) / 2.0 + f(b) / 2.0 + fxSum)
+module test = 
 
-
-let area = integrate 0.0 10.0 1000 xSquared
-printfn "Area = %f" area
-
+  let xSquared x : double = x * x
+  open integration
+  let area = integrate 0.0 100.0 1000 xSquared
+  printfn "Area = %f" area
